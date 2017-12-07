2e9e182dc376a4032755dd842eec60bc887ee403
JasonLWY <jasonlwy.163.com>
2017-12-07 22:29:03 +0800

    demo1

---
+++ vue_demo1/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" ref="stylesheet/stylus">
+    @import "common/stylus/variable.styl"
+    #app
+      color:$color-theme
+</style>
