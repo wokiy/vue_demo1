b7b64108c5f8401bf84d0a1e912687dcd74cc6d0
JasonLWY <jasonlwy.163.com>
2017-12-13 20:46:36 +0800

    music-list页面的渲染

--- vue_demo1/src/components/singer_detail/singer_detail.vue
+++ vue_demo1/src/components/singer_detail/singer_detail.vue
@@ -1,17 +1,15 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-    </div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
-
 </template>
 
 <script>
+  import MusicList from 'components/music-list/music-list'
   import {mapGetters} from 'vuex'
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
-
   export default {
     data() {
       return {
@@ -19,6 +17,12 @@
       }
     },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       //mapGetters 语法糖 函数仅仅是将 store 中的 getter 映射到局部计算属性
       /*mapGetters({
           // 映射 `this.Singer` 为 `store.getters.singer`
@@ -44,37 +48,32 @@
             console.log(this.songs)
           }
         })
-    },
+      },
       //整理歌手的数据格式显示前台的手段
-    _nomalizeSongs(list) {
-      //遍历list
-      let ret = []
-      list.forEach((item) => {
-        let {musicData} = item
-        //musicData 转化为 songs
-        if (musicData.songid && musicData.albummid) {
-          ret.push(createSong(musicData))
-        }
-      })
-      return ret
+      _nomalizeSongs(list) {
+        //遍历list
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          //musicData 转化为 songs
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
     }
-  }
   }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
 
-  .singer-detail
-    position: fixed
-    z-index 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
+  .slice-enter-active, .slide-leave-active
+    transition: all 0.3
 
-    .slice-enter-active, .slide-leave-active
-      transition: all 0.3
-    .slide-enter, .slide-leave-to
-      transition: transform3d(100%, 0, 0)
+  .slide-enter, .slide-leave-to
+    transition: transform3d(100%, 0, 0)
 </style>
